6e6d3d4d30c1a597a971bb9f91e43c366c43575d
liang <liang.huang@dci-student.org>
2025-10-24 15:15:06 +0200

    refractored the function into a class.

--- app/main.py
+++ app/main.py
@@ -1,26 +1,42 @@
 """The fizzbuzz program."""
 
 
-def fizz_buzz(n: int) -> list:
-    """Return a list of the fizzbuzz sequence up to n."""
-    try:
-        n = int(n)
-    except ValueError as e:
-        msg = "Input must be an integer"
-        raise ValueError(msg) from e
+class FizzBuzz:
+    """Class to handle the FizzBuzz logic."""
 
-    result: list = []
+    def __init__(self, n: int) -> None:
+        """Initialize the FizzBuzz class."""
+        self.n = n
 
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
-        else:
-            result.append(str(i))
-    return result
+    def check_input(self) -> None:
+        """Check the input to see if it is a positive integer."""
+        try:
+            self.n = int(self.n)
+        except ValueError as e:
+            msg = "Input must be an integer"
+            raise ValueError(msg) from e
+        if self.n <= 0:
+            msg = "Input must be a positive integer"
+            raise ValueError(msg)
+
+    def fizz_buzz(self) -> list:
+        """Return a list of the fizzbuzz sequence up to n."""
+        result: list = []
+
+        for i in range(1, self.n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
 
 
-print(fizz_buzz(15))
+if __name__ == "__main__":
+    n = input("Enter a positive integer: ")
+    fizz_buzz = FizzBuzz(n)
+    fizz_buzz.check_input()
+    print(fizz_buzz.fizz_buzz())
